e62d3304fb70c1dbaa5851ae9e7a92e5c44789dc
Zakaria Muse <zaknvf@Zakarias-MacBook-Air.local>
2023-10-10 11:56:56 -0500

    jQuery table with .each function used to color specfic list elements

---
+++ script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    console.log("ready"); 
+
+    $("td:nth-child(3)").each (function() {
+        if($(this).text() == "Complete") {
+            $(this).css("background-color", "lightgreen");
+        }else{
+            $(this).css("background-color", "red");
+        }
+    });
+});
